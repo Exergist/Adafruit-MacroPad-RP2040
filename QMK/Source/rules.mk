a83fbe5deea01093bafde1cd450b9450ff030a7b
Exergist <exergist.me@gmail.com>
2025-10-27 01:34:52 -0500

    Initial upload
    Adding original demo firmware and working baseline QMK source

---
+++ QMK/Source/rules.mk
@@ -0,0 +1,37 @@
+# MCU name
+#MCU = RP2040
+
+#Bootloader selection
+#BOOTLOADER = rp2040
+
+# Build Options
+#	Change to "no" to disable the options, or define them in the Makefile in
+#	the appropriate keymap folder that will get included automatically
+
+ENCODER_MAP_ENABLE = yes	# Enable rotary encoders
+BOOTMAGIC_ENABLE = no		# Virtual DIP switch configuration
+EXTRAKEY_ENABLE = yes		# Audio control and System control
+MOUSEKEY_ENABLE = no		# Mouse keys
+CONSOLE_ENABLE = no			# Console for debug
+COMMAND_ENABLE = no			# Commands for debug and configuration
+NKRO_ENABLE = no			# Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+BACKLIGHT_ENABLE = no		# Enable keyboard backlight functionality
+MIDI_ENABLE = no			# MIDI controls
+AUDIO_ENABLE = no			# Audio output on port C6
+UNICODE_ENABLE = yes		# Unicode
+BLUETOOTH_ENABLE = no		# Enable Bluetooth with the Adafruit EZ-Key HID
+#RGBLIGHT_ENABLE = yes		# Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE = yes
+TAP_DANCE_ENABLE = no		# Double press does something different
+RAW_ENABLE = yes			# Bidirectional communication between QMK and host computer via HID interface
+DEFERRED_EXEC_ENABLE = yes  # Enable ability to execute a callback after a specified period of time
+#OLED_ENABLE = yes			# Enable OLED functionality
+
+# Other Settings
+#OLED_TRANSPORT = spi		# Set OLED communication mechanism
+#AUDIO_DRIVER = pwm_hardware
+
+# Do not enable SLEEP_LED_ENABLE. It uses the same timer as BACKLIGHT_ENABLE.
+SLEEP_LED_ENABLE = no		# Enable breathing sleep for LEDs during USB suspend
+
+
